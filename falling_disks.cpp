9169178ef3766248c56ff7eccb04abdeb30fac95
hanson <hangzhong.yu@gmail.com>
2013-02-19 23:16:35 +0800

    falling disks with golden Codility Certificate (Omega 2013)

---
+++ falling_disks.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int falling_disks ( const vector<int> &A,const vector<int> &B ) {
+    int ans = 0;
+    int ib = 0;
+    int temp = A[0];
+    vector<int> tA;
+    for (int ia = 0; ia < A.size(); ++ia)
+    {
+        if (A[ia] < temp)
+        {
+            temp = A[ia];
+        }
+
+        tA.push_back(temp);
+    }
+
+    for (int ia = tA.size()-1; ia>=0 && ib<B.size(); --ia)
+    {
+       if (tA[ia] >= B[ib])
+       {
+            ++ib;
+            ++ans;
+       }
+    }
+
+    return ans;
+}
+
+int main()
+{
+    return 0;
+}
+
